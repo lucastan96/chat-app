0d3a7214703e0e3959da1041d8d54ea2ff7ab583
Lucas Tan <lucastan96@outlook.com>
2024-07-24 20:34:57 +0100

    Enable data binding in activity

--- app/src/main/java/com/lucastan/chat/MainActivity.kt
+++ app/src/main/java/com/lucastan/chat/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.lucastan.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set up data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Set up toolbar
+        setSupportActionBar(binding.toolbar)
     }
 }
